640c258fbb96e3355f2de45d9d15b9cfe3b2f8d4
sonia <soniafaye1@gmail.com>
2024-10-29 15:14:22 -0400

    dist folder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
 
 let people = [
@@ -30,6 +31,8 @@ morgan.token("body", function (req, res) {
 });
 
 app.use(express.json());
+app.use(cors);
+app.use(express.static("dist"));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
@@ -119,7 +122,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- notes.js
+++ notes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 let notes = [
@@ -23,6 +24,8 @@ let notes = [
 ];
 
 app.use(express.json());
+app.use(cors);
+app.use(express.static("dist"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
@@ -76,7 +79,7 @@ app.get("/api/notes/:id", (request, response) => {
   }
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
